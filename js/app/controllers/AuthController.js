eb935715d96016cf3bd381e25451c0790bba70eb
Piotr Niziniecki <pniziniecki@gmail.com>
2014-06-01 16:48:52 +0200

    Auth: little fix

--- js/app/controllers/AuthController.js
+++ js/app/controllers/AuthController.js
@@ -24,12 +24,8 @@ cloudScrum.controller('AuthController', function AuthController($scope, $rootSco
                         $rootScope.companies.push({id: files[i].id, name: files[i].title.replace('CloudScrum-', '')});
                     }
 
-                    if (files.length === 1) {
-                        Flow.setCompany(files[0].id, files[0].title.replace('CloudScrum-', ''));
-                        $location.path('/projects');
-                    } else {
-                        alert('todo! more than 1 company detected!'); //TODO
-                    }
+                    Flow.setCompany(files[0].id, files[0].title.replace('CloudScrum-', ''));
+                    $location.path('/projects');
                 }
             }, function(error) {
                 $rootScope.handleError(error);
